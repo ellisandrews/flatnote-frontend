dd09f7215fbb6d63618e11681736c795c8e758a4
ellisandrews <ellisandrews1@gmail.com>
2020-05-02 23:08:49 -0500

    Add some basic display routing

--- src/components/layout/Display.js
+++ src/components/layout/Display.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import Home from '../views/Home'
+import NewNote from '../views/NewNote'
+import Signout from '../views/Signout'
 
 
 class Display extends Component {
 
   render() {
     return (
-      <p>Welcome to FlatNote!</p>
+      <Switch>
+        <Route exact path="/notes/new">
+          <NewNote />
+        </Route>
+        <Route exact path="/signout">
+          <Signout />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     )
   }
 }
---
+++ src/components/views/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+
+
+class Home extends Component {
+  
+  render() {
+    return (
+      <Container>
+        <h1>HOME</h1>
+        <p>Welcome to FlatNote!</p>
+      </Container>
+    )
+  }
+}
+
+
+export default Home
