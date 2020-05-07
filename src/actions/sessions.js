0bd1a7dfc96948d89dd3b6f7e3d1d877f7352e9f
ellisandrews <ellisandrews1@gmail.com>
2020-05-07 13:10:08 -0500

    Very basic failed signup alerting

--- src/actions/sessions.js
+++ src/actions/sessions.js
@@ -20,12 +20,23 @@ const signup = (data, callback) => {
     // Then dispatch an action to add the user to redux state as logged in (and save their notes in one fell swoop)
     // Then execute the callback (likely redirecting to a new page).
     fetch('http://localhost:3000/users', req)
-      .then(resp => resp.json())
+      .then(resp => {
+        // Fetch doesn't error on status >= 400, so have to check resp.ok
+        if (resp.ok) { 
+          return resp.json()
+        }
+
+        // Error out on 400 codes
+        if (resp.status >= 400 && resp.status < 500) {
+          // TODO: Check the errors returned from the backend to customize this message
+          throw 'Invalid signup. Username may already be taken.'
+        }
+      })
       .then(user => {
         dispatch({ type: 'LOG_IN_USER', user })  // Store user's data (including notes!)
         callback()
       })
-      .catch(err => console.log("ERROR:", err))
+      .catch(err => window.alert(`ERROR: ${err}`))
   }
 }
 
